3d3966a3567625610d527951d63065f9cc40b84c
luanaberinguel <luanaberinguel@gmail>
2022-08-04 13:13:46 -0300

    Primeiros passos em Java. Projeto sobre Hora do Sitema.

---
+++ horadosistema/src/horadosistema/HoradoSistema.java
@@ -0,0 +1,17 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoradoSistema {
+
+	public static void main(String[] args) {
+		
+		Date relogio = new Date();
+		
+		System.out.println("A hora do sistema é: ");
+		System.out.println(relogio.toString());
+		
+
+	}
+
+}
